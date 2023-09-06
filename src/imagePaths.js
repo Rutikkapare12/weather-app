0e0ea8a7e15a1bdb2b418abd92012bbdfa774ae1
Rutik kapare <rutikkapare6340@gmail.com>
2023-09-06 22:58:06 +0530

    required changes

--- src/imagePaths.js
+++ src/imagePaths.js
@@ -1,9 +1,9 @@
 // imagePaths.js
 
-export const cloudImage = "./src/assets/cloud.png";
-export const clearImage = "./src/assets/clear.png";
-export const rainImage = "./src/assets/rain.png";
-export const hazeImage = "./src/assets/haze.png";
-export const mistImage = "./src/assets/mist.png";
-export const snowImage = "./src/assets/snow.png";
-export const notFoundImage = "./src/assets/404.png";
+export const cloudImage = "./assets/cloud.png";
+export const clearImage = "./assets/clear.png";
+export const rainImage = "./assets/rain.png";
+export const hazeImage = "./assets/haze.png";
+export const mistImage = "./assets/mist.png";
+export const snowImage = "./assets/snow.png";
+export const notFoundImage = "./assets/404.png";
